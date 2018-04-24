a3713691f9cef00a082e6290caeb9a62d9fb8900
Márki-Zay Ferenc <ferencmarkizay@gmail.com>
2018-04-24 19:40:19 +0200

    Widget for available printers

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QListWidgetItem
 
 from design.main_ui import Ui_MainWindow
+from design.printerlistitem_ui import Ui_PrinterListItem
+from printer_api import get_available_printers
 
 
 class PrinterMainWindow(QMainWindow):
@@ -10,9 +12,31 @@ class PrinterMainWindow(QMainWindow):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.available_printers_loaded(get_available_printers())
+
 
     def openOptionsWindow(self):
         pass
+
+    def available_printers_loaded(self, printers):
+        for printer in printers:
+            item = QListWidgetItem(self.ui.available_printer_list)
+            self.ui.available_printer_list.addItem(item)
+            item_widget = PrinterListItemWidget()
+            item_widget.load_data(printer)
+            item.setSizeHint(item_widget.sizeHint())
+            self.ui.available_printer_list.setItemWidget(item, item_widget)
+
+
+class PrinterListItemWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_PrinterListItem()
+        self.ui.setupUi(self)
+
+    def load_data(self, printer):
+        self.ui.printername.setText(printer['name'])
+        self.ui.printerdescription.setText(printer['description'])
 
 
 if __name__ == "__main__":
@@ -22,4 +46,3 @@ if __name__ == "__main__":
     widget.show()
 
     app.exec()
-
---
+++ printer_api.py
@@ -0,0 +1,3 @@
+def get_available_printers():
+    return [{'name': 'Test printer 1', 'description': 'This is the first test printer'},
+            {'name': 'Test printer 2', 'description': 'This is the second test printer'}]
