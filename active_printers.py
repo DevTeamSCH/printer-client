8fab19cf00107360822bf09c36b3d758cb75d25d
knoll0403 <knoll0403@gmail.com>
2018-05-23 18:22:51 +0200

    Add active_printers.py

---
+++ active_printers.py
@@ -0,0 +1,19 @@
+import json
+
+class ActivePrinters:
+    def __init__(self):
+        self.activePrinters = []
+
+    def load_from_file(self):
+        try:
+            with open('active_printers.json', 'r') as json_data:
+                d = json.load(json_data)
+                self.activePrinters = d['activePrinters']
+        except FileNotFoundError:
+            pass
+
+    def save_to_file(self):
+        with open('active_printers.json', 'w') as json_data:
+            json.dump(self.__dict__, json_data)
+        pass
+
